01facfd49189c5683f0836521092db60bedce1ea
AzizaHalder <aziza.halder@sap.com>
2024-06-10 17:32:42 +0200

    feature: add loading state when click new quote

--- script.js
+++ script.js
@@ -5,13 +5,26 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show loading state
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading state
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Show new Quote
 function newQuote() {
+  loading();
   // Pick random quote from apiQuotes array
-  //   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   //   Check if author field is An
   if (quote.author === "Anonymous") {
@@ -26,12 +39,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove("long-quote");
   }
-
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
 
   try {
@@ -56,5 +71,3 @@ twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 getQuotes();
-
-// localQuotes();
